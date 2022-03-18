3695e1c617a8755cf0e895c8f2bfadabc05f2c81
heatondev <35467354+heatondev@users.noreply.github.com>
2022-03-18 14:30:31 -0600

    Added UI and Data saving

--- WN22_TylerHeaton_Sandbox/Assets/Scripts/SavePrefs.cs
+++ WN22_TylerHeaton_Sandbox/Assets/Scripts/SavePrefs.cs
@@ -6,85 +6,103 @@ using TMPro;
 
 public class SavePrefs : MonoBehaviour
 {
+    //Text for displaying information
     public TextMeshProUGUI floatText;
     public TextMeshProUGUI intText;
-    int intToSave = 5;
-    float floatToSave = 10f;
+    public TextMeshProUGUI dataText;
+    public TextMeshProUGUI preamble;
 
+    //variables to be saved
+    int intToSave;
+    float floatToSave;
+    string stringToSave;
+
+    //inputs for changing variables
     public Slider floatSlider;
     public Slider intSlider;
+    public TMP_InputField nameString;
+
+    //complex variables
+    bool onShowObject = false;
+    Color saveObjectColor = Color.blue;
+    
 
     private void Start()
     {
-      ChangeSliderValue();
+     LoadGame();
     }
 
-    private void Update()
-    {
-       
-    }
 
     public void ChangeSliderValue()
     {
-        //string intMessage = intSlider.value.ToString();
-        // string floatMessage = floatSlider.value.ToString();
         floatToSave = floatSlider.value;
-        floatText.text = floatToSave.ToString();
         intToSave = Mathf.RoundToInt(intSlider.value);
         intText.text = intToSave.ToString();
+        floatText.text = floatToSave.ToString();
     }
-    //string stringToSave = "";
-
-    //void OnGUI()
-    //{
-    //    if (GUI.Button(new Rect(0, 0, 125, 50), "Raise Integer"))
-    //        intToSave++;
-    //    if (GUI.Button(new Rect(0, 100, 125, 50), "Raise Float"))
-    //        floatToSave += 0.1f;
-    //    //stringToSave = GUI.TextField(new Rect(0, 200, 125, 25),
-    //    //           stringToSave, 15);
-    //    GUI.Label(new Rect(375, 0, 125, 50), "Integer value is "
-    //               + intToSave);
-    //    GUI.Label(new Rect(375, 100, 125, 50), "Float value is "
-    //               + floatToSave.ToString("F1"));
-    //    //GUI.Label(new Rect(375, 200, 125, 50), "String value is "
-    //    //          + stringToSave);
-    //    if (GUI.Button(new Rect(750, 0, 125, 50), "Save Your Game"))
-    //        SaveGame();
-    //    if (GUI.Button(new Rect(750, 100, 125, 50),
-    //                "Load Your Game"))
-    //        LoadGame();
-    //    if (GUI.Button(new Rect(750, 200, 125, 50),
-    //                "Reset Save Data"))
-    //        ResetData();
-    //}
+  // saves playerpref keys and values
     public void SaveGame()
     {
+        intToSave = Mathf.RoundToInt(intSlider.value);
+        floatToSave = floatSlider.value;
+        stringToSave = nameString.text;
         PlayerPrefs.SetInt("SavedInteger", intToSave);
+        PlayerPrefs.SetInt("SavedInteger2", 5);
         PlayerPrefs.SetFloat("SavedFloat", floatToSave);
-      //  PlayerPrefs.SetString("SavedString", stringToSave);
+        PlayerPrefs.SetString("SavedString", stringToSave);
         PlayerPrefs.Save();
+        dataText.text = "Your number is " + intToSave.ToString();
         Debug.Log("Game data saved!");
     }
-
+    //loads values from playerprefs
    public void LoadGame()
     {
+        //check to see if key/value has been saved
         if (PlayerPrefs.HasKey("SavedInteger"))
         {
-            intToSave = PlayerPrefs.GetInt("SavedInteger");
+            intToSave = PlayerPrefs.GetInt("SavedInteger");       
             floatToSave = PlayerPrefs.GetFloat("SavedFloat");
-          // stringToSave = PlayerPrefs.GetString("SavedString");
+            stringToSave = PlayerPrefs.GetString("SavedString");
+          
+            dataText.text = "Hello " + stringToSave + ". Your integer was " + intToSave.ToString() + " and your float was " + floatToSave.ToString();
             Debug.Log("Game data loaded!");
         }
         else
-            Debug.LogError("There is no save data!");
+        { Debug.LogError("There is no save data!"); }
+        StartCoroutine(UpdateUI());
     }
+
+   
     public void ResetData()
     {
         PlayerPrefs.DeleteAll();
         intToSave = 0;
         floatToSave = 0.0f;
-       // stringToSave = "";
+        // stringToSave = "";
+        dataText.text = "Your number is " + intToSave.ToString();
+       StartCoroutine( UpdateUI());
+       
         Debug.Log("Data reset complete");
+    }
+
+    public void QuitGame()
+    {
+        SaveGame();
+        UnityEditor.EditorApplication.isPlaying = false;
+       // Application.Quit();
+    }
+    IEnumerator UpdateUI()
+    {
+         
+        intText.text = intToSave.ToString();
+        yield return new WaitForSeconds(0.1f);
+        floatText.text = floatToSave.ToString();
+            yield return new WaitForSeconds(0.1f);
+        intSlider.value = intToSave;
+            yield return new WaitForSeconds(0.1f);
+        floatSlider.value = floatToSave;
+        Debug.Log(floatToSave);
+        //preamble.text = "Enter Name";
+
     }
 }
